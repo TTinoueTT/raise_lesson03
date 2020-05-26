9edeb7b37ca2725f2e1542916816fa17d31c58d4
T_inoue_T <t.artwork.ceramica@gmail.com>
2020-05-27 00:21:30 +0900

    1. % bin/rails g controller tasks index show new edit 2. routes.rb編集  • resources :tasks の追加(他のルーティングコメントOFF)  • root to: 'tasks/index の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :tasks
+  root to: 'tasks/index'
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
