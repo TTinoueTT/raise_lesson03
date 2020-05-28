a89002e47740b7cfb4835f8077d35830bfdfab62
T_inoue_T <t.artwork.ceramica@gmail.com>
2020-05-28 18:26:44 +0900

    ### READまで 1. controllerにcreate/show/indexの中身記述&privateメソッド記述 2. application/index/new/showのviewを記入

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,6 +14,20 @@ class TasksController < ApplicationController
     #ブラウザに返す画面を指定できるが省略もできる、省略の場合はアクション名に対応したViewを返す
   end
 
+  def create
+    task = Task.new(task_params) #task_paramsで安全化された値を取得してTaskオブジェクトを作成
+    task.save! #taskインスタンスが保存されれば↓
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    #データを登録した後に登録時にいたページ以外ならredirectで指定URLに飛ぶ
+    #何も指定しなければrenderで元のページにもどる
+  end
+
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
