6844bc8674ba026c2f45dd70c9dae21b46c9bda1
T_inoue_T <t.artwork.ceramica@gmail.com>
2020-10-26 17:40:52 +0900

    add: has_manyメソッドの追加 user_idrが1に対して、task_idを多にすることを実現させる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  has_many :tasks
+  # このクラスのidを外部キーとして渡し、manyで渡された側で外部キーが複数登録可能
+  # 1 : 多 の実現
 end
